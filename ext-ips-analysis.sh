372756849b3aefd812a5b30c6f799415744198ca
Nir Forer <nirforer@gmail.com>
2019-11-18 13:35:14 +0200

    fix

--- ext-ips-analysis.sh
+++ ext-ips-analysis.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
+gcloud projects list --format="value(projectId)" > projects.txt
+if [ "$?" -ne 0 ]
+then
+	rm projects.txt
+	exit 1
+fi
 echo "Project	Instance_Name	External_Ip	Is_Preemptible	Monthly_Cost" > projectsIPs.csv
 
 while read project_to_list_ips; do
 	COMPUTE_API_ENABLED=$(gcloud services list --project=$project_to_list_ips | grep compute.googleapis.com | wc -l)
 	if [ "$COMPUTE_API_ENABLED" -eq "1" ] 
 	then
+		printf "Listing compute engine instances with external ip for project $project_to_list_ips that has the Compute Engine API Enabled\n"
 		gcloud compute instances list --flatten="networkInterfaces[].accessConfigs[]" --filter="networkInterfaces.accessConfigs.natIP:*" --format="value(name, networkInterfaces.accessConfigs.natIP, scheduling.preemptible)" --project $project_to_list_ips > temp.csv	
 		sed -i -e "s/^/$project_to_list_ips	/" temp.csv	
 	fi
-	cat temp.csv >> projectsIPs.csv
-done <projects
-rm temp.csv
+	if [ -f "./temp.csv" ]
+	then
+		cat temp.csv >> projectsIPs.csv		
+	fi
+done <projects.txt
+if [ -f "./temp.csv" ]
+then
+	rm temp.csv projects.txt
+else 
+	rm projects.txt
+fi
+
 
 INSTANCE_LINE=1
 while read instance; do
@@ -42,4 +58,6 @@ else
 	sed "s/false/false	2.92/" projectsIPs.csv
 	sed "s/true/true	1.46/" projectsIPs.csv
 fi
+
+printf "\nFinished, Please review the output over projectsIPs.csv\n"
 
